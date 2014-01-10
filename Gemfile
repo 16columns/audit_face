fc54dbe27557f1ab1b11355d0e68517071b95ef7
vishal agrawal <vishal.agr88@yahoo.com>
2014-01-10 17:15:22 +0530

    file upload

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ gem 'rails', '3.2.16'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
-gem 'mysql2', '~> 0.3.11'
+gem 'mysql2'
 # gem 'sqlite3-ruby'
 gem 'devise'
 gem 'simple_form'
@@ -16,6 +16,7 @@ gem 'date_validator'
 gem 'prawn'
 gem 'prawn-rails'
 gem 'bootstrap-datetimepicker-rails'
+gem 'cocaine'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/models/finding.rb
+++ app/models/finding.rb
@@ -2,13 +2,17 @@ class Finding < ActiveRecord::Base
   attr_accessible :category, :corrective_action, :date_created, :description, 
                   :preventive_action, :risk_rating, :audit_id, :department_name, :category_name, 
                   :risk_name, :status_name, :status_id,
-                  :document_attributes, :iso_clause
+                  :document_attributes, :iso_clause, :avatar
   belongs_to :audit
   belongs_to :finding_type
   belongs_to :risk
   belongs_to :finding_status
   belongs_to :document
+
+  has_attached_file :avatar #, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   
   accepts_nested_attributes_for :document
   
 end
+
+
