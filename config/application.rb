2ffd38a57a563297cbd3ea077e17f25d86c1c08f
vishant <vishant.pai@gmail.com>
2014-01-20 14:43:08 +0000

    audit status issue fixed

--- config/application.rb
+++ config/application.rb
@@ -38,7 +38,7 @@ module AuditFace
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
-
+    config.time_zone = 'Kolkata'
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
 
@@ -58,5 +58,6 @@ module AuditFace
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.assets.initialize_on_precompile = false
+    
   end
 end
