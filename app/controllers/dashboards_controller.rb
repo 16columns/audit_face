9043c8d8943d3fa78293b09cc7e291d04583ee4e
vishant <vishant.pai@gmail.com>
2014-02-09 07:12:08 +0000

    dashboard design change

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -6,7 +6,8 @@ class DashboardsController < ApplicationController
     
     @finding_count =0
     @open_audits_count = 0
-    @upcoming_audits = 0
+    @open_audits = []
+    @upcoming_audits = []
     
     @audits.each do |audit|
       audit.findings.each do|finding|
@@ -18,6 +19,7 @@ class DashboardsController < ApplicationController
       audit.findings.each do|finding|
         if finding.status_id != "Closed"
           @open_audits_count += 1
+          @open_audits << audit
           break
         end 
       end
@@ -26,7 +28,16 @@ class DashboardsController < ApplicationController
     @upcoming_audits = current_user.audits.where('start_date >= ?',Time.now)
     
     @findings = Finding.find(:all)
-    
+    months = ['Jan','Feb', 'Mar','Apr','May','Jun','Jul','Aug','Sep','Oct', 'Nov','Dec']
+    col_chart = [[]]
+    months.each_with_index do |month,index|
+      
+      col_chart[index] = Array.new
+      col_chart[index] = ['1','2','3']
+    end   
+    puts "**************this is the col chart**********"
+    puts col_chart
+    puts "***********************************************"
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @audits }
