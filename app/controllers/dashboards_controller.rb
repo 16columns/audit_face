60488a2c4672f288706855c7af15223598d8a161
vishant <vishant.pai@gmail.com>
2014-02-12 12:55:27 +0000

    google chart demo

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -28,16 +28,26 @@ class DashboardsController < ApplicationController
     @upcoming_audits = current_user.audits.where('start_date >= ?',Time.now)
     
     @findings = Finding.find(:all)
-    months = ['Jan','Feb', 'Mar','Apr','May','Jun','Jul','Aug','Sep','Oct', 'Nov','Dec']
-    col_chart = [[]]
-    months.each_with_index do |month,index|
+   # months = ['Jan','Feb', 'Mar','Apr','May','Jun','Jul','Aug','Sep','Oct', 'Nov','Dec']
+   # col_chart = [[]]
+   # months.each_with_index do |month,index|
       
-      col_chart[index] = Array.new
-      col_chart[index] = ['1','2','3']
-    end   
-    puts "**************this is the col chart**********"
-    puts col_chart
-    puts "***********************************************"
+  #    col_chart[index] = Array.new
+  #    col_chart[index] = ['1','2','3']
+  #  end   
+  #  puts "**************this is the col chart**********"
+  #  puts col_chart
+  #  puts "***********************************************"
+    # testing data from index for google col chart
+    #dataTable.addColumn(["one", "two", "three","four"]);
+    #    <% current_user.audits.each do |audit| %>
+    #          dataTable.addRow([<%= audit.id %>,<%= audit.id %> ,<%= audit.id %> ,<%= audit.id %>]) 
+    #    <%end%>  
+    #      alert("*************"+dataTable);
+    audits = Audit.all
+    #audits.each do |audit|
+    #  @data_table.add_row([audit.created_at, audit.start_date, audit.start_date, audit.start_date])
+    #end
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @audits }
