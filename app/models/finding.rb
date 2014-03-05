704bec872b7a07651d4160b5c5074848c0d94046
vishant <vishant.pai@gmail.com>
2014-03-05 14:33:50 +0000

    validation on closure date

--- app/models/finding.rb
+++ app/models/finding.rb
@@ -9,6 +9,7 @@ class Finding < ActiveRecord::Base
   belongs_to :finding_status
   # belongs_to :document
    has_many :documents, :dependent => :destroy
+  validates :closure_date, :date => {:after_or_equal_to => :created_at, :message => 'must be after or same as created date' }
   validates_presence_of :corrective_action, :if => Proc.new { |finding| finding.status_id == "Under review" || finding.status_id == "Closed"} ,:message => "Please enter CAPA before you change the status to Under Review"
 #  has_attached_file :avatar #, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   
