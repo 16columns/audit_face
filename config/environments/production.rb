fa5cf34d02d78696cea33e96ff0103ff01a2ef94
vishal <vishalagr2008@gmail.com>
2014-04-15 21:59:17 +0530

    mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,14 +66,16 @@ AuditFace::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  ActionMailer::Base.smtp_settings = {  
-  :address              => "smtp.gmail.com",  
-  :port                 => 587,  
-  :domain               => "gmail.com",  
-  :user_name            => "noreply77comply@gmail.com",  
-  :password             => "comply77",  
-  :authentication       => "plain",  
-  :enable_starttls_auto => true  
+  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'http://auditface.herokuapp.com' }
+      ActionMailer::Base.smtp_settings = {  
+      :address              => "oxmail.registrar-servers.com",  
+      :port                 => 25,  
+      :domain               => "77comply.com",  
+      :user_name            => "support@77comply.com",  
+        :password             => "W!nner999",  
+      :authentication       => "plain",  
+      :enable_starttls_auto => true  
 }  
-end
+  end
